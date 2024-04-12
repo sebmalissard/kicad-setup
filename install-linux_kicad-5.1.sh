85615bdc16722e122a04f06a100da3a0efda27e4
Sebastien Malissard <sebastien.malissard@dbmail.com>
2024-04-13 01:03:15 +0200

    Update KiCad version to 8.0

--- install-linux_kicad-5.1.sh
+++ install-linux_kicad-5.1.sh
@@ -22,12 +22,17 @@ kicad-install()
         exit 1
     fi
 
+    if awk "BEGIN {exit !($(lsb_release -r | awk '{print $2}') > 21.10)}"; then
+        echo "Max Ubuntu version supported 21.10"
+        exit 1
+    fi
+
     if command -v kicad; then
         echo "Kicad is already installed. Abort!"
         exit 1
     fi
     
-    sudo add-apt-repository --yes ppa:kicad/kicad-8.0-releases &&
+    sudo add-apt-repository --yes ppa:kicad/kicad-5.1-releases &&
     sudo apt update &&
     sudo apt install --no-install-recommends kicad
     if [ "$?" != "0" ]; then
@@ -45,46 +50,37 @@ libraries-install()
         exit 1
     fi
     
-    git clone https://github.com/sebmalissard/kicad-symbols    -b staging_v8 "${INSTALL_DIR}/kicad-symbols"
-    git clone https://github.com/sebmalissard/kicad-footprints -b staging_v8 "${INSTALL_DIR}/kicad-footprints"
-    git clone https://github.com/sebmalissard/kicad-packages3D -b staging_v8 "${INSTALL_DIR}/kicad-packages3D"
+    git clone https://github.com/sebmalissard/kicad-symbols     "${INSTALL_DIR}/kicad-symbols"
+    git clone https://github.com/sebmalissard/kicad-footprints  "${INSTALL_DIR}/kicad-footprints"
+    git clone https://github.com/sebmalissard/kicad-packages3D  "${INSTALL_DIR}/kicad-packages3D"
 }
 
 env-install()
 {
-    if [ -e "${HOME}/.config/kicad/8.0/fp-lib-table" ]; then
-        echo "Kicad was already installed. Need to delete this file: '${HOME}/.config/kicad/8.0/fp-lib-table'. Abort!"
+    if [ -e "${HOME}/.config/kicad/fp-lib-table" ]; then
+        echo "Kicad was already installed. Need to delete this file: '${HOME}/.config/kicad/fp-lib-table'. Abort!"
         exit 1
     fi
 
-    if [ -e "${HOME}/.config/kicad/8.0/sym-lib-table" ]; then
-        echo "Kicad was already installed. Need to delete this file: '${HOME}/.config/kicad/8.0/sym-lib-table'. Abort!"
+    if [ -e "${HOME}/.config/kicad/sym-lib-table" ]; then
+        echo "Kicad was already installed. Need to delete this file: '${HOME}/.config/kicad/sym-lib-table'. Abort!"
         exit 1
     fi
     
-    if ! grep -q "kicad_env.sh" "${HOME}/.bashrc"; then
-        echo "" >> "${HOME}/.bashrc"
-        echo ". ~/.config/kicad/kicad_env.sh" >> "${HOME}/.bashrc"
+    if [ -e "${HOME}/.config/kicad/kicad_common" ]; then
+        sed -i "s|KISYSMOD=.*|KISYSMOD=\"${INSTALL_DIR_ABS}/kicad-footprints\"|g" "${HOME}/.config/kicad/kicad_common"
+        sed -i "s|KISYS3DMOD=.*|KISYS3DMOD=\"${INSTALL_DIR_ABS}/kicad-packages3D\"|g" "${HOME}/.config/kicad/kicad_common"
+        sed -i "s|KICAD_SYMBOL_DIR=.*|KICAD_SYMBOL_DIR=\"${INSTALL_DIR_ABS}/kicad-symbols\"|g" "${HOME}/.config/kicad/kicad_common"
+    else
+        mkdir -p "${HOME}/.config/kicad"
+        echo "[EnvironmentVariables]"                                 > "${HOME}/.config/kicad/kicad_common"
+        echo "KISYSMOD=\"${INSTALL_DIR_ABS}/kicad-footprints\""      >> "${HOME}/.config/kicad/kicad_common"
+        echo "KISYS3DMOD=\"${INSTALL_DIR_ABS}/kicad-packages3D\""    >> "${HOME}/.config/kicad/kicad_common"
+        echo "KICAD_SYMBOL_DIR=\"${INSTALL_DIR_ABS}/kicad-symbols\"" >> "${HOME}/.config/kicad/kicad_common"
     fi
-
-    if [ -e "${HOME}/.config/kicad/kicad_env.sh" ]; then
-        rm "${HOME}/.config/kicad/kicad_env.sh"
-    fi
-
-    mkdir -p "${HOME}/.config/kicad/8.0"
-
-    echo "#!/bin/sh"                                                             > "${HOME}/.config/kicad/kicad_env.sh"
-    echo ""                                                                     >> "${HOME}/.config/kicad/kicad_env.sh"
-    echo "export KICAD8_FOOTPRINT_DIR=\"${INSTALL_DIR_ABS}/kicad-footprints\""  >> "${HOME}/.config/kicad/kicad_env.sh"
-    echo "export KICAD8_3DMODEL_DIR=\"${INSTALL_DIR_ABS}/kicad-packages3D\""    >> "${HOME}/.config/kicad/kicad_env.sh"
-    echo "export KICAD8_SYMBOL_DIR=\"${INSTALL_DIR_ABS}/kicad-symbols\""        >> "${HOME}/.config/kicad/kicad_env.sh"
     
-    ln -s "${INSTALL_DIR_ABS}/kicad-footprints/fp-lib-table" "${HOME}/.config/kicad/8.0/fp-lib-table"
-    ln -s "${INSTALL_DIR_ABS}/kicad-symbols/sym-lib-table" "${HOME}/.config/kicad/8.0/sym-lib-table"
-
-    echo "Before launch KiCad source new environment  variables"
-    echo ". ~/.bashrc"
-    echo ""
+    ln -s "${INSTALL_DIR_ABS}/kicad-footprints/fp-lib-table" "${HOME}/.config/kicad/fp-lib-table"
+    ln -s "${INSTALL_DIR_ABS}/kicad-symbols/sym-lib-table" "${HOME}/.config/kicad/sym-lib-table"
 }
 
 projects-install()
